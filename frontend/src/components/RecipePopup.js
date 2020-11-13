2b0406bbf1837b94e86c76bb19ac68914d555f96
John Tomikawa <john.tomikawa@gmail.com>
2020-11-12 16:15:30 -0800

    Adding recipe popup layout. Interface with backend still missing.

--- frontend/src/components/RecipePopup.js
+++ frontend/src/components/RecipePopup.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
 import './styles.css';
 
 import { PopupContext } from "../context/popup-context";
@@ -8,14 +9,75 @@ class RecipePopup extends React.Component {
 static contextType = PopupContext;
 
   render() {
+    const ingredientColSize = 8;
     let context = this.context;
     return (
-      <div className='popup'>
-        <div className='popup\_inner'>
-          <h1>"Test"</h1>
-            <button onClick={() => context.recipeButtonClicked()}>Close</button>
+        <div className='popup'>
+          <Form>
+            <Form.Row>
+              <Col>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Control size="lg" type="recipe_name" placeholder="Recipe" className="text-center"/>
+                </Form.Group>
+              </Col>
+              <Col>
+              </Col>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} xs={ingredientColSize}>
+                <Form.Label>Ingredient</Form.Label>
+                <Form.Control type="ingredient" placeholder="Ingredient" />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Qty</Form.Label>
+                <Form.Control type="quantity" placeholder="0"/>
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Units</Form.Label>
+                <Form.Control type="units" placeholder="oz"/>
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} xs={ingredientColSize}>
+                <Form.Control type="ingredient" placeholder="Ingredient" />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control type="quantity" placeholder="0"/>
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control type="units" placeholder="oz"/>
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} xs={ingredientColSize}>
+                <Form.Control type="ingredient" placeholder="Ingredient" />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control type="quantity" placeholder="0"/>
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control type="units" placeholder="oz"/>
+              </Form.Group>
+            </Form.Row>
+
+            <div class="text-left">
+            <Button variant="primary" type="add_ingredient">
+              Add ingredient
+            </Button>
+            </div>
+            <div class="text-right">
+            <Button variant="primary" type="save" onClick={() => context.recipeButtonClicked()}>
+              Save
+            </Button>
+            </div>
+          </Form>
+            {/*}<button onClick={() => context.recipeButtonClicked()}>Close</button>*/}
         </div>
-      </div>
     );
   }
 }
